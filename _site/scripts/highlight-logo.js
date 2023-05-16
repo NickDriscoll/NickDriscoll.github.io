f6606f54495b81e148335895ab855b342ec285e0
Nick Driscoll <nick.a.mavros@gmail.com>
2023-05-16 13:03:01 -0400

    I can't read

--- _site/scripts/highlight-logo.js
+++
@@ -1,6 +0,0 @@
-let headerGitIcons = document.querySelectorAll(".git");
-
-headerGitIcons.forEach((icon) => {
-    icon.addEventListener("mouseover", e => icon.src = "images/git-full.svg");
-    icon.addEventListener("mouseout", e => icon.src = "images/git-empty.svg");
-});
